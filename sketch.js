e0e920e15ac3b38ababc8c185958217b5ebd92bc
AgasthyaChikka <85887314+AgasthyaChikka@users.noreply.github.com>
2021-06-21 20:03:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+var ball;
+var ground;
+var con;
+
+
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  
+  world = engine.world;
+
+    var ball_options = {
+                restitution:0.8
+    }
+    ball=Bodies.circle(200,  50,  20,  ball_options)
+    World.add(world, ball)
+
+    ball2=Bodies.circle(350,10,22,ball_options)
+    World.add(world,ball2)
+    con = Matter.Constraint.create({
+        pointA: { x: 200, y: 20 },
+        bodyB: ball,
+        length: 100,
+        stiffness: 0.1
+    });
+
+    con2 = Matter.Constraint.create({
+        bodyA:  ball,
+        bodyB:  ball2,
+        length:  100,
+        stiffness:0.1
+    })
+    World.add(world,con)
+    World.add(world,con2)
+    
+}
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+    ellipse(ball.position.x, ball.position.y, 20, 20)
+ellipse(ball2.position.x,ball2.position.y,22,22)
+
+          stroke(255)
+      line (con.pointA.x,con.pointA.y,ball.position.x,ball.position.y)
+      line (ball.position.x,ball.position.y,ball2.position.x,ball2.position.y)
+
+}
+
+function keyPressed() {
+    if (keyCode === RIGHT_ARROW) {
+        Matter.Body.applyForce(ball, { x: 0, y: 0 }, {x:0.05,y:0})
+    }
+}
+
+
